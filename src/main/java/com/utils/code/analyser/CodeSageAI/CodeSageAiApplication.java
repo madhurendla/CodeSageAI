9dba95e6678491631f08c71a84c940d081d740ce
Madhu Rendla <madhu.rendla27@gmail.com>
2026-04-30 07:40:37 +0530

    This is the CodeSageAI - Initial commit

---
+++ src/main/java/com/utils/code/analyser/CodeSageAI/CodeSageAiApplication.java
@@ -0,0 +1,13 @@
+package com.utils.code.analyser.CodeSageAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSageAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeSageAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utils/code/analyser/CodeSageAI/CodeSageAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utils.code.analyser.CodeSageAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSageAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
